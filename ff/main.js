745e59bf3a3f061fbcef338c4df9249fa58f891c
Lucas Youngers <lucas00y@uw.edu>
2024-04-15 11:02:33 -0700

    separated into two scripts

--- ff/main.js
+++ ff/main.js
@@ -3,12 +3,4 @@ navigator.__defineGetter__("userAgent", function () {
     return "test 1"
 })
 
-// seems to work on amiunique
-chrome.webRequest.onBeforeSendHeaders.addListener(details => {
-    details.requestHeaders
-        .filter(({ name }) => name === "User-Agent")
-        .forEach(header => header.value = "test 2")
-    return { requestHeaders: details.requestHeaders };
-}, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
-
 console.log(navigator.userAgent)
