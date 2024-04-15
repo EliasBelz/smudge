c09e84d0dfed3b01747aa09e9e7116fafba39a86
Lucas Youngers <lucas00y@uw.edu>
2024-04-15 10:48:59 -0700

    header smudging now works for user-agent on amiunique

--- ff/main.js
+++ ff/main.js
@@ -1,8 +1,14 @@
-// document.body.style.border = "5px solid blue"
-
+// seems to not work on amiunique
 navigator.__defineGetter__("userAgent", function () {
-    return 'test'
+    return "test 1"
 })
 
-// alert(window.navigator.userAgent)
-console.log(window.navigator.userAgent)
+// seems to work on amiunique
+chrome.webRequest.onBeforeSendHeaders.addListener(details => {
+    details.requestHeaders
+        .filter(({ name }) => name === "User-Agent")
+        .forEach(header => header.value = "test 2")
+    return { requestHeaders: details.requestHeaders };
+}, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
+
+console.log(navigator.userAgent)
