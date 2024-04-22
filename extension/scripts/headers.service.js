c4ca65540c7cdc2df2e20d7501f465f35f2c658d
eliasbelzberg <ebelz@uw.edu>
2024-04-22 11:54:37 -0700

    changed user agent in header

--- extension/scripts/headers.service.js
+++ extension/scripts/headers.service.js
@@ -2,6 +2,6 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(details => {
     details.requestHeaders
         .filter(({ name }) => name === "User-Agent")
-        .forEach(header => header.value = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.3");
+        .forEach(header => header.value = "Fake user agent header");
     return { requestHeaders: details.requestHeaders };
 }, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
