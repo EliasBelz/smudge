93f7bf037827cd3aee459596ce1699d7765b1991
eliasbelzberg <ebelz@uw.edu>
2024-05-13 14:14:55 -0700

    removed user agent toggle

--- extension/popup/index.js
+++ extension/popup/index.js
@@ -44,12 +44,19 @@
 
         for (let key of Object.keys(data)) {
             const options = data[key];
-
             const selectEl = document.createElement('select');
             selectEl.id = key;
+            // updates the settings when the dropdown is changed.
+            // if the platform or browser is changed, the user agent is updated.
             selectEl.addEventListener('change', async function () {
-                console.log(`Updated ${key} to ${this.value}`);
-                await settingsService.updateSettings(key, this.value);
+                if (key != 'userAgent ') {
+                    await settingsService.updateSettings(key, this.value);
+                }
+                if (key === 'platform' || key === 'browser') {
+                    const { platform, browser } = await settingsService.getSettings();
+                    const userAgent = getUserAgent({ platform, browser });
+                    await settingsService.updateSettings('userAgent', userAgent);
+                }
             });
 
             if (navigator[key]) {
@@ -66,6 +73,7 @@
                 selectEl.appendChild(opt);
             }
 
+            // set the dropdown to the current setting or to the first option.
             const currentSetting = (await settingsService.getSettings())[key];
             if (currentSetting && options.includes(currentSetting)) {
                 selectEl.value = currentSetting;
@@ -92,5 +100,57 @@
             browser.tabs.reload();
         });
     }
+
+    function getUserAgent({platform, browser}) {
+        if (!browser || !platform) {
+          return null;
+        }
+
+        return makeUAString({platform, browser});
+      }
+
+      function makeUAString({platform, browser}) {
+        return `Mozilla/5.0 (${getSystemInfo(platform)}) ${getBrowserInfo(browser)}`;
+      }
+
+      /**
+       * Defaults to Linux x86_64
+       * @param {string} param0
+       * @returns
+       */
+      function getSystemInfo(platform) {
+        switch (platform) {
+          case 'Windows':
+            return 'Windows NT 10.0; Win64; x64';
+          case 'Macintosh':
+            return 'Macintosh; Intel Mac OS X 10.15; rv:125.0';
+          default:
+            return `${platform}; Linux x86_64`;
+        }
+
+      }
+
+      /**
+       * Returns the system information and extension for a given browser.
+       * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+       * @param {string} browser - browser name
+       * @returns string - browser info
+       */
+      function getBrowserInfo(browser) {
+        switch (browser) {
+          case 'Chrome':
+            return 'AppleWebKit/537.36 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36';
+          case 'Firefox':
+            return 'Gecko/20100101 Firefox/68.0';
+          case 'Opera':
+            return 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41';
+          case 'Microsoft Edge':
+            return 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59';
+          case 'Safari':
+            return 'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15';
+          default:
+            return '';
+        }
+      }
 
 })();
--- extension/scripts/navigator.service.js
+++ extension/scripts/navigator.service.js
@@ -9,11 +9,8 @@
         const script = document.createElement('script');
         const {
             platform,
-            browser
+            userAgent
         } = settings;
-        const userAgent = makeUserAgent({ platform, browser });
-        settings.userAgent = userAgent;
-        await browser.storage.local.set({ settings: { ...settings } });
         console.log(`extension UA ${userAgent}`);
         script.textContent = `
             Object.defineProperty(navigator, 'platform', {
@@ -36,56 +33,4 @@
         (document.head || document.documentElement).appendChild(script);
         script.remove();
     }
-
-    function makeUserAgent({platform, browser}) {
-        if (!browser || !platform) {
-          return null;
-        }
-
-        return makeUAString({platform, browser});
-      }
-
-      function makeUAString({platform, browser}) {
-        return `Mozilla/5.0 (${getSystemInfo(platform)}) ${getBrowserInfo(browser)}`;
-      }
-
-      /**
-       * Defaults to Linux x86_64
-       * @param {string} param0
-       * @returns
-       */
-      function getSystemInfo(platform) {
-        switch (platform) {
-          case 'Windows':
-            return 'Windows NT 10.0; Win64; x64';
-          case 'Macintosh':
-            return 'Macintosh; Intel Mac OS X 10.15; rv:125.0';
-          default:
-            return `${platform}; Linux x86_64`;
-        }
-
-      }
-
-      /**
-       * Returns the system information and extension for a given browser.
-       * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
-       * @param {string} browser - browser name
-       * @returns string - browser info
-       */
-      function getBrowserInfo(browser) {
-        switch (browser) {
-          case 'Chrome':
-            return 'AppleWebKit/537.36 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36';
-          case 'Firefox':
-            return 'Gecko/20100101 Firefox/68.0';
-          case 'Opera':
-            return 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41';
-          case 'Microsoft Edge':
-            return 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59';
-          case 'Safari':
-            return 'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15';
-          default:
-            return '';
-        }
-      }
 })();
